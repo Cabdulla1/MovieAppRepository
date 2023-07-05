caa61a96bf9628b5eed7adf039c30a526c560560
Cavidan Abdulla <cavidan442@gmail.com>
2023-07-05 16:07:56 +0400

    Commit 1

---
+++ MovieWebApp/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MovieWebApp.Data;
+using MovieWebApp.Models;
+
+namespace MovieWebApp.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly MovieWebApp.Data.MovieWebAppContext _context;
+
+        public IndexModel(MovieWebApp.Data.MovieWebAppContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Movie != null)
+            {
+                Movie = await _context.Movie.ToListAsync();
+            }
+        }
+    }
+}
